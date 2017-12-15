23bba49f27e93833e92b0f01ae4c3add5a5794d8
rematchka <reem.abdelsalam13@gmail.com>
2017-12-15 22:13:52 +0200

    pla almost done some changes added in src decoder

--- BranchInstructionDecoder.vhd
+++ BranchInstructionDecoder.vhd
@@ -29,10 +29,35 @@ Port
 end Entity;
 
 Architecture BranchInstructionDecoderArch of BranchInstructionDecoder is
+
+component decoder is
+ Generic (n : integer :=4);
+Port(   En:in std_logic; 
+	s: in std_logic_vector(n-1 downto 0); 
+	output: out std_logic_vector(((2**n)-1) downto 0)
+     );
+end component;
+
+signal BRDecoderOut: std_logic_vector( 7 downto 0);
+signal BREN: std_logic;
+signal JSR:  std_logic;
+
+
 begin
+
+
 
 --TODO:
 --Implement branching circuit as in description above
+BREN <= IRinput(3) and IRinput(4);
+
+BranchDecoder : decoder generic map (n   => 3) port map(BREN,IRinput(3 downto 0),BRDecoderOut);
+
+JSR <= IRinput(4) and (not (IRinput(3))) and (not (IRinput(2)))and IRinput(1) and IRinput(0);
+
+ Branch <= JSR or BRDecoderOut(0) or (BRDecoderOut(1) and ZF) or (BRDecoderOut(2) and (not ZF)) or (BRDecoderOut(3) and CF) or (((not CF) or ZF) and BRDecoderOut(4)) or (BRDecoderOut(5) and CF)or ((CF or ZF) and BRDecoderOut(5));
+
+ --   
 
 end BranchInstructionDecoderArch;
 
--- InstructionDecodingCircuit.vhd
+++ InstructionDecodingCircuit.vhd
@@ -1,5 +1,6 @@
-Library ieee;
-use ieee.std_logic_1164.all;
+LIBRARY IEEE;
+USE IEEE.STD_LOGIC_1164.ALL;
+USE IEEE.numeric_std.all;
 
 --Description
 --------------
@@ -72,10 +73,115 @@ begin
 ALUDecoder:decoder generic map (n   => 4) port map(ALUEN,IRinput(13 downto 10),aluDecoderOutput);
 -------------------------------------------------SRC---------------------------------------------------------------------------------------
 
-SRCMUX: MUX2 generic map (n   => 2)  port map(IRinput(1 downto 0),IRinput(6 downto 5),SRCEND,SRCDecoderInput);
+SRCMUX: MUX2 generic map (n   => 2)  port map(IRinput(9 downto 8),IRinput(5 downto 4),SRCEND,SRCDecoderInput);
 SRCDecoder:decoder generic map (n   => 2) port map(RSRCEN,SRCDecoderInput,srcDecoderOutput);
 -------------------------------------------------DST---------------------------------------------------------------------------------------
-DSTMUX: MUX2 generic map (n   => 2)  port map(IRinput(6 downto 5),IRinput(1 downto 0),DSTENS,DSTDecoderInput);
+DSTMUX: MUX2 generic map (n   => 2)  port map(IRinput(9 downto 8),IRinput(5 downto 4),DSTENS,DSTDecoderInput);
 DSTDecoder:decoder generic map (n   => 2) port map(RDSTEN,DSTDecoderInput,dstDecoderOutput);
 ------------------------------------------------PLA----------------------------------------------------------------------------------------
+PROCESS(PLAout) IS
+BEGIN
+If PLAout = '1' then
+-------------------------------if mov operation-------------------------------------------------------------------------------------------
+ if IRinput(15 downto 10)="001001" then
+---------- src=direct------------dst=direct----------------------------------
+ 
+  if IRinput(5 downto 4)="00" and IRinput(9 downto 8)="00" then
+   PLAoutput<=std_logic_vector(TO_UNSIGNED (58 ,7));
+  END IF;
+------------------------src=direct ------dst=indexed-------------------------
+  if IRinput(5 downto 4)="11" and IRinput(9 downto 8)="00" then
+   PLAoutput<=std_logic_vector(TO_UNSIGNED (46 ,7));
+  END IF;
+--------------------------src=AI---------dst=indx----------------------------
+   if IRinput(5 downto 4)="11" and IRinput(9 downto 8)="01" then
+   PLAoutput<=std_logic_vector(TO_UNSIGNED (49 ,7));
+  END IF;
+---------------------------src=AD-----------dst=indexed-------------------
+  if IRinput(5 downto 4)="11" and IRinput(9 downto 8)="10" then
+   PLAoutput<=std_logic_vector(TO_UNSIGNED (54 ,7));
+  END IF;
+----------------------------src=direct-----------------------------
+if  IRinput(9 downto 8)="00" then
+   PLAoutput<=std_logic_vector(TO_UNSIGNED (17 ,7));
+  END IF;
+
+ END IF;
+---------------------------if cmp direct direct -----------------------------------------------------------------------------------------
+if IRinput(15 downto 10)="000100" and IRinput(5 downto 4)="00" and IRinput(9 downto 8)="00"  then
+   PLAoutput<=std_logic_vector(TO_UNSIGNED (59 ,7));
+  END IF;
+--------------------------------------if one operand direct -----------------------------------------------------------------------------
+ if IRinput(15 downto 14)="01" and  IRinput(9 downto 8)="00" then 
+ PLAoutput<=std_logic_vector(TO_UNSIGNED (61 ,7));
+  END IF;
+--------------------------------------------------no op---------------------------------------------------------------------------------
+if IRinput(15 downto 10)="10001" then
+PLAoutput<=std_logic_vector(TO_UNSIGNED (10 ,7));
+  END IF;
+---------------------------------------------------RST-----------------------------------------------------------------------------------
+if IRinput(15 downto 10)="10010" then
+PLAoutput<=std_logic_vector(TO_UNSIGNED (11 ,7));
+  END IF;
+---------------------------------------------Single opreand not direct-------------------------------------------------------------------
+if IRinput(15 downto 14)="01" then
+
+---------------------------if AI-------------------------------------------------
+if IRinput(9 downto 8)="01" then 
+PLAoutput<=std_logic_vector(TO_UNSIGNED (20 ,7));
+  END IF;
+----------------------------if AD-----------------------------------------------
+if IRinput(9 downto 8)="10" then 
+PLAoutput<=std_logic_vector(TO_UNSIGNED (24 ,7));
+  END IF;
+-------------------------if indexed------------------------------------------
+if IRinput(9 downto 8)="11" then 
+PLAoutput<=std_logic_vector(TO_UNSIGNED (28 ,7));
+  END IF;
+
+END IF;
+-------------------------------if double operand -----------------------------------------------------------------------------------------
+if IRinput(15 downto 14)="00" then
+
+-------------------if src= direct-----------------------------------
+if IRinput(9 downto 8)="00" then 
+PLAoutput<=std_logic_vector(TO_UNSIGNED (16 ,7));
+  END IF;
+
+
+-------------------if src=AI---------------------------
+if IRinput(9 downto 8)="01" then 
+PLAoutput<=std_logic_vector(TO_UNSIGNED (20 ,7));
+  END IF;
+-----------------------if src=AD----------------------
+
+if IRinput(9 downto 8)="10" then 
+PLAoutput<=std_logic_vector(TO_UNSIGNED (24 ,7));
+  END IF;
+---------------------------if src=indexed-------------
+if IRinput(9 downto 8)="11" then 
+PLAoutput<=std_logic_vector(TO_UNSIGNED (28 ,7));
+  END IF;
+
+
+
+ END IF;
+  ----------------------------------------JSR----------------------------------------------------------------------------------------------
+if IRinput(15 downto 10)="10011" then
+PLAoutput<=std_logic_vector(TO_UNSIGNED (4,7));
+  END IF;
+-----------------------------------------RTS-----------------------------------------------------------------------------------------------
+if IRinput(15 downto 10)="10100" then
+PLAoutput<=std_logic_vector(TO_UNSIGNED (7,7));
+  END IF;
+----------------------------------------------any branch-----------------------------------------------------------------------------------
+if IRinput(15 downto 14)="11" then
+PLAoutput<=std_logic_vector(TO_UNSIGNED (63,7));
+  END IF;
+
+ END IF;
+  
+END PROCESS;
+
+
 end InstructionDecodingCircuitArch;
